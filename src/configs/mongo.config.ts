32a7faef0026b5ccb90dcbdcdd688d5bf2db9a4a
Dang Tinh <caodangtinh04022003@gmail.com>
2024-10-28 13:04:12 +0700

    update: code base

---
+++ src/configs/mongo.config.ts
@@ -0,0 +1,48 @@
+import dotenv from "dotenv";
+dotenv.config({ path: [".env.local", ".env"] });
+
+interface Config {
+  app: {
+    port: number;
+    host: string;
+  };
+  db: {
+    url: string;
+    appName: string;
+    dbName: string;
+  };
+}
+
+const dev: Config = {
+  app: {
+    port: Number(process.env.DEV_APP_PORT) || 3000,
+    host: process.env.DEV_APP_HOST ?? "localhost",
+  },
+  db: {
+    url: process.env.MONGO_DB_URL!,
+    appName: process.env.MONGO_DB_APP_NAME!,
+    dbName: process.env.MONGO_DB_NAME!,
+  },
+};
+
+const prod = {
+  app: {
+    port: Number(process.env.PROD_APP_PORT) || 3000,
+    host: process.env.PROD_APP_HOST ?? "localhost",
+  },
+  db: {
+    url: process.env.MONGO_DB_URL,
+    appName: process.env.MONGO_DB_APP_NAME,
+    dbName: process.env.MONGO_DB_NAME,
+  },
+};
+
+type Env = "dev" | "prod";
+
+const configs: Record<Env, Config | typeof prod> = {
+  dev,
+  prod,
+};
+
+const env: Env = (process.env.NODE_ENV as Env) ?? "dev";
+export default configs[env];
---
+++ src/dbs/init.mongodb.ts
@@ -0,0 +1,42 @@
+import mongoose from "mongoose";
+import config from "../configs/mongo.config";
+
+const dbConnection = `mongodb+srv://${config.db.url}/${config.db.dbName}?retryWrites=true&w=majority/&appName=${config.db.appName}`;
+
+class Database {
+  constructor() {
+    this.connect();
+  }
+
+  public static instance: Database;
+
+  connect(type = "mongo") {
+    switch (type) {
+      case "mongo":
+        mongoose
+          .connect(dbConnection)
+          .then(() => {
+            console.log("Connected to MongoDB");
+          })
+          .catch((error) => {
+            console.log("Error connecting to MongoDB");
+            console.error(error);
+          });
+        break;
+      case "mysql":
+        break;
+      default:
+        break;
+    }
+  }
+
+  static getInstance() {
+    if (!Database.instance) {
+      Database.instance = new Database();
+    }
+    return Database.instance;
+  }
+}
+
+const instanceMongodb = Database.getInstance();
+export default instanceMongodb;
