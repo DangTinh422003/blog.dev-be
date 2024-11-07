a20d7e3e4b77bc8ee879d00c9e4f0b10c9fb253f
dangtinh <caodangtinh04022003@gmail.com>
2024-11-07 16:12:55 +0700

    fix: lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,7 @@ export default [
     rules: {
       '@typescript-eslint/no-explicit-any': 'warn',
       '@typescript-eslint/no-unused-vars': 'warn',
+      '@typescript-eslint/no-unsafe-function-type': 0,
     },
     ignores: ['**/node_modules/', '**/dist/'],
   },
